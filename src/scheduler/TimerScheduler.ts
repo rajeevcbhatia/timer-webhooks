1e2184be295a513e2e9bea0bf15b699bc3fab9bf
Rajeev Chandra Bhatia <rajeevcbhatia@gmail.com>
2023-09-10 12:31:56 +0200

    error handling for post requests

--- src/scheduler/TimerScheduler.ts
+++ src/scheduler/TimerScheduler.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import TimerDetailsRepository from '../db/repository/TimerDetailsRepository'
 import { ObjectId } from 'mongodb'
 
@@ -10,7 +10,35 @@ export class TimerScheduler {
       console.log(timer)
       if (timer && !timer.isFired) {
         console.log('TIMER FIRED')
-        await axios.post(`${webhookURL}/${timerId}`)
+        try {
+          await axios.post(`${webhookURL}/${timerId}`)
+        } catch (error) {
+          /*
+          Error handling strategies:-
+            1. Retrying failed requests
+            2. Logging and monitoring 
+            3. storing post status in DB for audit trails
+           */
+          const axiosError = error as AxiosError
+          if (axiosError.response) {
+            // Response was received with error
+            console.error(
+              'Server responded with an error:',
+              axiosError.response.data,
+            )
+          } else if (axiosError.request) {
+            // No response but request was made
+            console.error(
+              'No response received from the server:',
+              axiosError.request,
+            )
+          } else {
+            // Something went wrong with the request
+            console.error('Error setting up the request:', axiosError.message)
+          }
+          // Log more details if needed
+          console.error(axiosError.config)
+        }
       }
     }, timeLeft * 1000)
   }
